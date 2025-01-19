6b4bca261f5473726927b9a28e6db4e1b4a7468b
Tejasbabar24 <tejasbabar864@gmail.com>
2025-01-19 20:20:52 +0530

    two pages addded

---
+++ frontend/src/components/ProductsCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductsCards() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProductsCards
---
+++ frontend/src/components/ProductsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductsPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProductsPage
