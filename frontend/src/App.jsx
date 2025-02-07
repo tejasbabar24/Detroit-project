970088f5ecd323fe470d34e1e0b93acec48d8653
SwapnilShinde20 <shindeswapnil2005@gmail.com>
2025-02-07 16:24:52 +0530

    z2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import LandingPage from './LandingPage'
+import { useDispatch } from 'react-redux'
+import { login, logout } from './components/store/authSlice.js'
+import { getUser } from './api/user'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()  
+  
+  useEffect(()=>{
+    getUser()
+    .then((userData)=>{      
+      if(userData){
+        dispatch(login(userData.data.data))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+    
+  },[])
+  return !loading ? (
     <>
       <LandingPage/>
     </>
-  )
+  ) : null
 }
 
 export default App
