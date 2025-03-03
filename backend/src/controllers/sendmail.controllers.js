97248ac6cccc26819ecfdd614e357f22ccb142f7
SwapnilShinde20 <shindeswapnil2005@gmail.com>
2025-03-03 12:25:02 +0530

    email servuce done

--- backend/src/controllers/sendmail.controllers.js
+++ backend/src/controllers/sendmail.controllers.js
@@ -4,12 +4,13 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail", // Use Gmail (or another SMTP service)
+  host: "smtp.zoho.com", // Zoho SMTP Host
+  port: 465, // Secure Port for Zoho SMTP
+  secure: true, // Use SSL
   auth: {
-    user: process.env.EMAIL_USER, // Your email address
-    pass: process.env.EMAIL_PASS, // Your email password or App Password
+    user: process.env.EMAIL_USER, // Zoho Email ID
+    pass: process.env.EMAIL_PASS, // Zoho App Password
   },
-  
 });
 const sendEmail = asyncHandler(async (req, res, next) => {
 
@@ -21,17 +22,18 @@ const sendEmail = asyncHandler(async (req, res, next) => {
   
  
   const mailOptions = {
-    from: senderEmail, // Sender's email (user's input)
+    from: process.env.EMAIL_USER, // Sender's email (user's input)
     to: process.env.RECEIVER_EMAIL, // Your email to receive messages
     subject: `New Contact Form Submission from ${senderName}`,
     text: `Name: ${senderName}\nEmail: ${senderEmail}\nPhone: ${senderPhone}\nMessage: ${text}`,
     replyTo: senderEmail, // Allows you to reply directly to the user
   };
   
-    const info = await transporter.sendMail(mailOptions)
-    if(!info.response){
+    const response = await transporter.sendMail(mailOptions)
+    if(!response || !response.accepted.length){
       return next(new ApiError(500,"", "Failed to send message. Try again later."));
     }
+    
     return res
         .status(200)
         .json(new ApiResponse(200, "", "Form Submitted ! Our agent will contact you soon !!"));
